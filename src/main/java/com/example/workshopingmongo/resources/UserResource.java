1c5a164915b816d8b0a3bf45c103b967f8cd1066
adm <kaio.candf@gmail.com>
2025-11-18 16:41:27 -0300

    conectando mongodb e ajustando serviços

--- src/main/java/com/example/workshopingmongo/resources/UserResource.java
+++ src/main/java/com/example/workshopingmongo/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.example.workshopingmongo.resources;
 
+import com.example.workshopingmongo.services.UserService;
 import domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +18,13 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     //@RequestMapping(method= RequestMethod.GET)
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
-        User maria = new User("1", "maria", "maria@gmail.com");
-        User alex = new User("2", "alex", "alex@gmail.com");
-
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
---
+++ src/main/java/com/example/workshopingmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.example.workshopingmongo.services;
+
+import com.example.workshopingmongo.repository.UserRepository;
+import domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+    
+}
