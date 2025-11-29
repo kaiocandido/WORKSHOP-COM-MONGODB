d564c7624543239ec2351c1df03c99b7ad4b2089
adm <kaio.candf@gmail.com>
2025-11-28 21:31:11 -0300

    finalizado projeto com busca de diversos comentarios, corpo, comentario, data

--- src/main/java/com/example/workshopingmongo/repository/PostRepository.java
+++ src/main/java/com/example/workshopingmongo/repository/PostRepository.java
@@ -5,6 +5,7 @@ import domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
@@ -14,5 +15,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
     @Query("{'title': { '$regex': ?0, '$options': 'i' } }")
     List<Post> searchTitle(String text);
 
+    @Query("{$and: [ { date: {$gte: ?1} }, { date: {$lte: ?2} }, { $or: [ {'title': { '$regex': ?0, '$options': 'i' } } , {'body': { '$regex': ?0, '$options': 'i' } }, {'comments.text': { '$regex': ?0, '$options': 'i' } } ] } ]}")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
--- src/main/java/com/example/workshopingmongo/resources/PostResource.java
+++ src/main/java/com/example/workshopingmongo/resources/PostResource.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -44,4 +45,21 @@ public class PostResource {
 
         return ResponseEntity.ok().body(list);
     }
+
+    @RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate
+    ) {
+        text = URL.decodeParam(text);
+
+        Date min = URL.DateConvert(minDate, new Date(0L));
+        Date max = URL.DateConvert(minDate, new Date());
+        List<Post> list = service.fullSearch(text, min, max);
+
+        return ResponseEntity.ok().body(list);
+    }
+
+
 }
